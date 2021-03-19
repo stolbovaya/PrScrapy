7cf0aa876ceeb7513ac033e5d9f0b3cb7deca524
stolbovaya <stolbovaya@yahoo.com>
2021-03-19 16:19:00 +0300

    lesson8 for one account

--- instaparser/spiders/instagram.py
+++ instaparser/spiders/instagram.py
@@ -14,7 +14,7 @@ class InstagramSpider(scrapy.Spider):
     allowed_domains = ['instagram.com']
     start_urls = ['https://instagram.com/']
     insta_login = 'kasws3'
-    insta_pwd = '#PWD_INSTAGRAM_BROWSER'
+    insta_pwd = ''
     inst_login_link = 'https://www.instagram.com/accounts/login/ajax/'
     parse_user = 'fushik1'  # ,'3dprintus']      #Пользователь, у которого собираем посты. Можно указать список
 
@@ -39,49 +39,12 @@ class InstagramSpider(scrapy.Spider):
 
         if j_body['authenticated']:  # Проверяем ответ после авторизации
             # for parse_user in self.parse_users:
+
             yield response.follow(
                 # Переходим на желаемую страницу пользователя. Сделать цикл для кол-ва пользователей больше 2-ух
                 f'/{self.parse_user}',
                 callback=self.user_data_parse,
                 cb_kwargs={'username': self.parse_user})
-
-            yield response.follow(
-                # Переходим на желаемую страницу пользователя. Сделать цикл для кол-ва пользователей больше 2-ух
-                f'/{self.parse_user}',
-                callback=self.followers_data_parse,
-                cb_kwargs={'username': self.parse_user})
-
-            yield response.follow(
-                # Переходим на желаемую страницу пользователя. Сделать цикл для кол-ва пользователей больше 2-ух
-                f'/{self.parse_user}',
-                callback=self.following_data_parse,
-                cb_kwargs={'username': self.parse_user})
-
-    def followers_data_parse(self, response: HtmlResponse, username):
-        user_id = self.fetch_user_id(response.text, username)  # Получаем id пользователя
-        variables = {'id': user_id,  # Формируем словарь для передачи даных в запрос
-                     'first': 12}  # 12 постов. Можно больше (макс. 50)
-        url_posts = f'{self.graphql_url}query_hash={self.followers_hash}&{urlencode(variables)}'  # Формируем ссылку для получения данных о постах
-        yield response.follow(
-            url_posts,
-            callback=self.followers_posts_parse,
-            cb_kwargs={'username': username,
-                       'user_id': user_id,
-                       'variables': deepcopy(variables)}  # variables ч/з deepcopy во избежание гонок
-        )
-
-    def following_data_parse(self, response: HtmlResponse, username):
-        user_id = self.fetch_user_id(response.text, username)  # Получаем id пользователя
-        variables = {'id': user_id,  # Формируем словарь для передачи даных в запрос
-                     'first': 12}  # 12 постов. Можно больше (макс. 50)
-        url_posts = f'{self.graphql_url}query_hash={self.following_hash}&{urlencode(variables)}'  # Формируем ссылку для получения данных о постах
-        yield response.follow(
-            url_posts,
-            callback=self.following_posts_parse,
-            cb_kwargs={'username': username,
-                       'user_id': user_id,
-                       'variables': deepcopy(variables)}  # variables ч/з deepcopy во избежание гонок
-        )
 
     def user_data_parse(self, response: HtmlResponse, username):
         user_id = self.fetch_user_id(response.text, username)  # Получаем id пользователя
@@ -91,6 +54,24 @@ class InstagramSpider(scrapy.Spider):
         yield response.follow(
             url_posts,
             callback=self.user_posts_parse,
+            cb_kwargs={'username': username,
+                       'user_id': user_id,
+                       'variables': deepcopy(variables)}  # variables ч/з deepcopy во избежание гонок
+        )
+
+        url_posts = f'{self.graphql_url}query_hash={self.following_hash}&{urlencode(variables)}'  # Формируем ссылку для получения данных о постах
+        yield response.follow(
+            url_posts,
+            callback=self.following_posts_parse,
+            cb_kwargs={'username': username,
+                       'user_id': user_id,
+                       'variables': deepcopy(variables)}  # variables ч/з deepcopy во избежание гонок
+        )
+
+        url_posts = f'{self.graphql_url}query_hash={self.followers_hash}&{urlencode(variables)}'  # Формируем ссылку для получения данных о постах
+        yield response.follow(
+            url_posts,
+            callback=self.followers_posts_parse,
             cb_kwargs={'username': username,
                        'user_id': user_id,
                        'variables': deepcopy(variables)}  # variables ч/з deepcopy во избежание гонок
@@ -121,8 +102,6 @@ class InstagramSpider(scrapy.Spider):
                            'variables': deepcopy(variables)}
             )
 
-
-
     def followers_posts_parse(self, response: HtmlResponse, username, user_id,
                               variables):  # Принимаем ответ. Не забываем про параметры от cb_kwargs
         j_data = json.loads(response.text)
@@ -137,7 +116,6 @@ class InstagramSpider(scrapy.Spider):
 
             )
             yield item  # В пайплайн
-
         page_info = j_data.get('data').get('user').get('edge_followed_by').get('page_info')
         if page_info.get('has_next_page'):  # Если есть следующая страница
             variables['after'] = page_info['end_cursor']  # Новый параметр для перехода на след. страницу
@@ -149,8 +127,6 @@ class InstagramSpider(scrapy.Spider):
                            'user_id': user_id,
                            'variables': deepcopy(variables)}
             )
-
-
 
     def following_posts_parse(self, response: HtmlResponse, username, user_id,
                               variables):  # Принимаем ответ. Не забываем про параметры от cb_kwargs
@@ -177,8 +153,6 @@ class InstagramSpider(scrapy.Spider):
                            'variables': deepcopy(variables)}
             )
 
-
-
     # Получаем токен для авторизации
     def fetch_csrf_token(self, text):
         matched = re.search('\"csrf_token\":\"\\w+\"', text).group()
@@ -190,8 +164,8 @@ class InstagramSpider(scrapy.Spider):
             matched = re.search(
                 '{\"id\":\"\\d+\",\"username\":\"%s\"}' % username, text
             ).group()
-            id =json.loads(matched).get('id')
+            id = json.loads(matched).get('id')
         except Exception as e:
-            id ='0'
+            id = '0'
             print(e)
         return id
